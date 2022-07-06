6336294152168ff3eb2552196bcb8f5111047175
Brenton Andrews <brenton.j.andrews@gmail.com>
2022-07-06 09:07:39 -0600

    Commit before deploy

--- app.js
+++ app.js
@@ -19,6 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Router paths.
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -26,6 +28,8 @@ app.use('/users', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hello there, I am a dog but I can type I guess.",
+    user: "Ginger",
+    added: new Date()
+  },
+  {
+    text: "I am also a dog, woof",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express Message Board', messages : messages});
 });
+
+router.get("/new", function(req, res, next) {
+  res.render('form', { title : "Form!"});  
+})
+
+router.post("/new", (req, res, next) => {
+  console.log(req.body.name);
+  let msg_name = req.body.name;
+  let msg_contents = req.body.message;
+  messages.push({ text : msg_contents, user : msg_name, added: new Date()})
+  res.redirect("/");
+})
 
 module.exports = router;
